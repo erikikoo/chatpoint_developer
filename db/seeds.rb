c9723121942ca315115261a92ddcf26b4d529c39
Erik R. de Souza <erikikoo@hotmail.com>
2016-11-03 16:52:05 -0200

    'AddSeed'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Client.create(cliente: '2h', cidade: 'Guarulhos', bairro: 'Cumbica', contato: 'Erik', fone:'997899896', status: true, url: 'www.teste.com.br/2h', username: 'erikikoo', password: '123456')
